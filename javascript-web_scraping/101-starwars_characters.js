936d10a7a1498ff42991df80e12a10568cb2eaa7
SimonDesc <pro.descombes@gmail.com>
2023-09-19 18:18:08 +0200

    fix: semistandard

--- javascript-web_scraping/101-starwars_characters.js
+++ javascript-web_scraping/101-starwars_characters.js
@@ -11,39 +11,39 @@ You must use the module request
 ./100-starwars_characters.js 3
 */
 
-const request = require("request");
-const URL = "https://swapi-api.hbtn.io/api/films/" + process.argv[2];
+const request = require('request');
+const URL = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
-request(URL, function(error, response, body) {
-    if (error) {
+request(URL, function (error, response, body) {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  const obj = JSON.parse(body);
+
+  const listCharacters = obj.characters;
+
+  const charactersName = {};
+  let completedRequests = 0;
+  const totalRequests = listCharacters.length;
+
+  for (const characterUrl of listCharacters) {
+    request(characterUrl, function (error, response, body) {
+      if (error) {
         console.error(error);
         return;
-    }
-    const obj = JSON.parse(body);
+      } else {
+        const id = characterUrl.match(/(\d+)\/?$/); // expression reguliere qui retourne un tableau avec l'id en 1
+        const objCharacters = JSON.parse(body);
+        charactersName[id[1]] = objCharacters.name;
+      }
 
-    const listCharacters = obj.characters;
-
-    const charactersName = {};
-    let completedRequests = 0;
-    const totalRequests = listCharacters.length;
-
-    for (const characterUrl of listCharacters) {
-        request(characterUrl, function(error, response, body) {
-            if (error) {
-                console.error(error);
-                return;
-            } else {
-                const id = characterUrl.match(/(\d+)\/?$/); // expression reguliere qui retourne un tableau avec l'id en 1
-                const objCharacters = JSON.parse(body);
-                charactersName[id[1]] = objCharacters.name;
-            }
-
-            completedRequests++;
-            if (completedRequests === totalRequests) {
-                for (const cle in charactersName) {
-                    console.log(charactersName[cle]);
-                }
-            }
-        });
-    }
+      completedRequests++;
+      if (completedRequests === totalRequests) {
+        for (const cle in charactersName) {
+          console.log(charactersName[cle]);
+        }
+      }
+    });
+  }
 });
