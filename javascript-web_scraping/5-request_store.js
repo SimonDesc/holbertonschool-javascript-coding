03aa09d133a8b849ba247b01623eaffa0fdb30f0
SimonDesc <pro.descombes@gmail.com>
2023-09-19 10:41:57 +0200

    fix: without console log its better

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -8,19 +8,17 @@ The file must be UTF-8 encoded
 You must use the module request
 */
 
-// ./5-request_store.js http://loripsum.net/api loripsum
-const request = require("request");
-const fs = require("fs");
+const request = require('request');
+const fs = require('fs');
+
 const URL = process.argv[2];
 const filePath = process.argv[3];
 
-console.log(filePath);
-
-request(URL, (error, response, body) => {
+request(URL, function (error, response, body) {
   if (error) {
     console.error(error);
   } else {
-    fs.writeFile(filePath, body, "utf-8", (err) => {
+    fs.writeFile(filePath, body, 'utf-8', (err) => {
       if (err) {
         console.error(err);
       }
